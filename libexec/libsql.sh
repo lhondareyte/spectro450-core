b78b2e4eba0e75ff7c01a9021a3addeacc869484
Luc Hondareyte <luc.hondareyte@laposte.net>
2022-10-01 08:45:11 +0200

    Add listConfigFiles ()

--- libexec/libsql.sh
+++ libexec/libsql.sh
@@ -7,10 +7,23 @@
 
 export pkgdb="/var/db/pkg/local.sqlite"
 export spectrodb="/etc/spectro.conf"
-export sql="/usr/local/bin/sqlite3"
-
+export sql="sqlite3"
+export PATH=$PATH:/usr/local/bin
 
 listApplications () {
-	$sql $pkgdb "select name from packages where prefix ='/Applications';" \
-		| awk ' { printf("%s\n",toupper(substr($0,1,1))substr($0,2)) }'
+	$sql $pkgdb "
+	SELECT name FROM packages 
+	WHERE prefix ='/Applications';" \
+	| awk ' { printf("%s\n",toupper(substr($0,1,1))substr($0,2)) }'
 }
+
+listConfigFiles () {
+	local app=$1
+	$sql $pkgdb "
+	SELECT path FROM files
+	WHERE path LIKE '%/etc/%.conf' AND package_id = (
+		SELECT id FROM packages
+		WHERE name = '${app}' AND prefix = '/Applications'
+	);"
+}
+
